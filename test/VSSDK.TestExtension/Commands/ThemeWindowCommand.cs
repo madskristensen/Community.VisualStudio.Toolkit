22dbee04b3fcc50522f1f1dc3a7b48d7b8267d7f
reduckted <reduckted@outlook.com>
2021-04-03 17:36:11 +1000

    Created a tool window and dialog to demonstrate and test theming.

---
+++ test/VSSDK.TestExtension/Commands/ThemeWindowCommand.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Shell.Interop;
+using Community.VisualStudio.Toolkit;
+using Task = System.Threading.Tasks.Task;
+
+namespace TestExtension
+{
+    internal sealed class ThemeWindowCommand : BaseCommand<ThemeWindowCommand>
+    {
+        public ThemeWindowCommand() : base(new Guid("cb765f49-fc35-4c14-93af-bb48ca4f2ce3"), 0x0101)
+        { }
+
+        protected override async Task ExecuteAsync(OleMenuCmdEventArgs e)
+        {
+            ToolWindowPane window = await Package.ShowToolWindowAsync(typeof(ThemeWindow), 0, true, CancellationToken.None);
+
+            if ((null == window) || (null == window.Frame))
+            {
+                throw new NotSupportedException("Cannot create tool window");
+            }
+
+            await ThreadHelper.JoinableTaskFactory.SwitchToMainThreadAsync();
+            var windowFrame = (IVsWindowFrame)window.Frame;
+            Microsoft.VisualStudio.ErrorHandler.ThrowOnFailure(windowFrame.Show());
+        }
+    }
+}
---
+++ test/VSSDK.TestExtension/ToolWindows/ThemeWindow.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using Microsoft.VisualStudio.Shell;
+
+namespace TestExtension
+{
+    [Guid("e3be6dd3-f017-4d6e-ae88-2b29319a77a2")]
+    public class ThemeWindow : ToolWindowPane
+    {
+        public const string Title = "Theme Window";
+
+        public ThemeWindow(ThemeWindowControlViewModel viewModel) : base(null)
+        {
+            Caption = Title;
+            Content = new ThemeWindowControl { DataContext = viewModel };
+        }
+    }
+}
